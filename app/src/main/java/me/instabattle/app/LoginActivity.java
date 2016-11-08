3a7f78d63448b31f46367126db3a31dd4871ba5d
wackloner <wackloner@gmail.com>
2016-11-08 23:36:51 +0300

    add LoginActivity, SignupActivity, MenuActivity, LoginTools

--- app/src/main/java/me/instabattle/app/LoginActivity.java
+++ app/src/main/java/me/instabattle/app/LoginActivity.java
@@ -1,13 +1,53 @@
 package me.instabattle.app;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
+    private EditText _email;
+    private EditText _password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        _email = (EditText) findViewById(R.id.email_input);
+        _password = (EditText) findViewById(R.id.password_input);
+    }
+
+    private boolean login(String email, String password) {
+        //TODO
+        return true;
+    }
+
+    public void onSigninClick(View view) {
+        String email = _email.toString();
+        String password = _password.toString();
+
+        if (!LoginTools.correctEmail(email)) {
+            Toast.makeText(this, "Incorrect email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!LoginTools.correctPassword(password)) {
+            Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (login(email, password)) {
+            Intent menu = new Intent(this, MenuActivity.class);
+            startActivity(menu);
+        }
+    }
+
+    public void onSignupClick(View view) {
+        Intent signup = new Intent(this, SignupActivity.class);
+        startActivity(signup);
     }
 }
---
+++ app/src/main/java/me/instabattle/app/LoginTools.java
@@ -0,0 +1,17 @@
+package me.instabattle.app;
+
+/**
+ * Created by wackloner on 08.11.2016.
+ */
+
+public class LoginTools {
+    public static boolean correctEmail(String email) {
+        //TODO
+        return true;
+    }
+
+    public static boolean correctPassword(String password) {
+        //TODO
+        return true;
+    }
+}
