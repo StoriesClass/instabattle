dc1794782abf11bb533d776a68f94a892b8b16ce
wackloner <wackloner@gmail.com>
2016-11-21 17:50:12 +0300

    add map

--- app/src/main/java/me/instabattle/app/MapActivity.java
+++ app/src/main/java/me/instabattle/app/MapActivity.java
@@ -1,13 +1,42 @@
 package me.instabattle.app;
 
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MapActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        UiSettings settings = mMap.getUiSettings();
+        settings.setMyLocationButtonEnabled(true);
+        settings.setZoomControlsEnabled(true);
+
+        LatLng spb = new LatLng(59.95, 30.32);
+
+        mMap.addMarker(new MarkerOptions().position(spb).title("Marker in SPb"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(spb, 13));
     }
 }
